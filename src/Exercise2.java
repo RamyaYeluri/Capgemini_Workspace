27163777a6e716f876b5da907ef156fd5f40a8a3
RamyYeluri <yeluriramya2409@gmail.com>
2021-01-07 14:43:59 +0530

    Lab 1

---
+++ src/Exercise2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str; 
+		System.out.println("select color 1. red 2.yellow 3.green");
+		
+		Scanner sc=new Scanner(System.in);
+		str=sc.next();
+		
+        switch(str) 
+        { 
+            case "red": 
+                System.out.println("stop"); 
+                break; 
+            case "yellow": 
+                System.out.println("ready"); 
+                break; 
+            case "green": 
+                System.out.println("go"); 
+                break; 
+            
+
+	}
+	}
+}
