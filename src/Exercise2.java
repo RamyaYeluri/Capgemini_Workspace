9c1b2f085463664f47c5368b67d470f342de2d85
RamyaYeluri <77097988+RamyaYeluri@users.noreply.github.com>
2021-01-11 08:59:58 +0530

    Update Exercise2.java

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -2,30 +2,31 @@
 *Select color and print output
 * Switch case
 */
-import java.util.*;
+import java.util.Scanner;
 public class Exercise2 {
+
+	private static void signaloutput( String n)
+	{
+		if(n.equalsIgnoreCase("red"))
+			System.out.println("stop");
+		
+		else 
+			if(n.equalsIgnoreCase("yellow"))
+			System.out.println("wait");
+		else
+			if(n.equalsIgnoreCase("green"))
+			System.out.println("go");
+		
+			
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str; 
-		System.out.println("select color 1. red 2.yellow 3.green");
-		
-		Scanner sc=new Scanner(System.in);
-		str=sc.next();
-		
-        switch(str) 
-        { 
-            case "red": 
-                System.out.println("stop"); 
-                break; 
-            case "yellow": 
-                System.out.println("ready"); 
-                break; 
-            case "green": 
-                System.out.println("go"); 
-                break; 
-            
 
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Code : ");
+		String code = sc.next();
+		signaloutput(code);
+		sc.close();
 	}
 }
